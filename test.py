36f33c2493c10dff9ea249cc834f0a3a6e553445
Kris Akwa-Mensah <krisakwamens@gmail.com>
2025-03-15 00:52:24 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-from google import genai
-from google.genai import types
-
-# Only run this block for Google AI API
-client = genai.Client(api_key='AIzaSyDP-GeJxcWWG6JE4vIC_BFJalgpDbuM2ZE')
